447e9d0b1a8c1887dfa0950004d6379089ef8a70
Mohamed Elgaraihy <mohamedelgaraihy2029@gmail.com>
2024-09-02 02:17:44 +0300

    added a new button in index

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
     include 'includes/header.php'; 
 ?>
     <h1 style="color: blue"> Home Page </h1>
+    <button> Click </button> <br>
     <!-- footer is included from includes/footer.php -->
     <?php include 'includes/footer.php' ?>
